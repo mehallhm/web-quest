4c9c809a5b225d23bb6c8242849f207887a4c63b
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-10-05 20:53:11 -0600

    Start problem & subview

--- web-check/src/App.js
+++ web-check/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Problem from './subcomp/problem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div class="title">
+          <h1>WebQuest</h1>
+        </div>
       </header>
+
+      <p>ds</p>
+      <p>fdafs</p>
     </div>
   );
 }
---
+++ web-check/src/subcomp/problem.js
@@ -0,0 +1,13 @@
+import './problem.css';
+
+function Problem(num) {
+  return (
+    <div className="Problem">
+      <header>
+        <button id="details">Details</button>
+      </header>
+    </div>
+  );
+}
+
+export default Problem;
