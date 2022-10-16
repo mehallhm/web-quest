280fe45b32b6d0cc1933cd3afd0558692542eb64
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-10-15 22:23:12 -0600

    More reorganization

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,12 @@ import "./App.css";
 import Problem from "./subcomp/problem";
 import React from "react";
 import Navbar from "./subcomp/navbar";
+import SetView from "./SetView";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
-      <div className="mt-20 grid grid-cols-9 grid-rows-1">
-        <div className="col-span-7 col-start-2">
-          <Problem
-            num={1}
-            qcontent={
-              "This is an example question. Something would go here. Something very important. Something to make an individual question reality..."
-            }
-          />
-        </div>
-      </div>
+      <SetView />
     </div>
   );
 }
---
+++ src/components/SetView.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import Problem from "./subcomp/problem";
+import React from "react";
+import Navbar from "./subcomp/navbar";
+
+function SetView() {
+  return (
+    <div className="SetView">
+      <Navbar />
+
+      <div className="mt-20 grid grid-cols-9 grid-rows-1">
+        <div className="col-span-7 col-start-2">
+          <Problem
+            num={1}
+            qcontent={
+              "This is an example question. Something would go here. Something very important. Something to make an individual question reality..."
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SetView;
