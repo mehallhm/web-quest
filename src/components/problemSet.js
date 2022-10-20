548f5196f9fcc7d8f0e93796f453860b6fab255e
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-10-20 13:41:53 -0600

    fm

--- src/components/problemSet.js
+++ src/components/problemSet.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import "../index.css";
+import firebaseConfig from "../creds.js";
 import Problem from "./problem";
-  
+
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
@@ -14,11 +21,11 @@ const q = query(collection(db, "questions"), where("units", "==", "m/s"));
 const getData = async () => {
   let result;
   let querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      result = doc.data().question;
+  querySnapshot.forEach((doc) => {
+    result = doc.data().question;
   });
   return result;
-}
+};
 
 // Initialize Firebase
 
@@ -26,22 +33,22 @@ function ProblemSet() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-     // Fetching Data on Initial Load
-     getData().then((data) => setData(data));
-  },[])
+    // Fetching Data on Initial Load
+    getData().then((data) => setData(data));
+  }, []);
 
   if (data == null) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <Problem num={0} qcontent={data}/>
-	  <Problem num={0} qcontent={data}/>
-	  <Problem num={0} qcontent={data}/>
-	  <Problem num={0} qcontent={data}/>
-	  <Problem num={0} qcontent={data}/>
-	  <Problem num={0} qcontent={data}/>
+      <Problem num={0} qcontent={data} />
+      <Problem num={0} qcontent={data} />
+      <Problem num={0} qcontent={data} />
+      <Problem num={0} qcontent={data} />
+      <Problem num={0} qcontent={data} />
+      <Problem num={0} qcontent={data} />
     </div>
   );
 }
